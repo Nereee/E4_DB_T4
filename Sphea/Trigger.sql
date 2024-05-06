80512b1ced999322887090294600a1a6d8d064a3
Aitzolgy <157578476+Aitzolgy@users.noreply.github.com>
2024-05-06 13:59:33 +0200

    Trigerrak estadistika totalak betetzeko

--- Sphea/Trigger.sql
+++ Sphea/Trigger.sql
@@ -6,3 +6,29 @@ BEFORE INSERT ON Bezeroa
 FOR EACH ROW
     set new.ErregistroData = CURDATE();
 //
+
+DELIMITER //
+
+DROP TRIGGER IF EXISTS EstadistikaTotalaBete //
+CREATE TRIGGER EstadistikaTotalaBete
+BEFORE INSERT ON EstadistikakEgunean
+FOR EACH ROW
+BEGIN
+    UPDATE EstadistikaTotala
+    SET Entzunaldiak =  Entzunaldiak + 1
+    WHERE IdAudio = NEW.IdAudio; 
+END;
+//
+
+DELIMITER //
+
+DROP TRIGGER IF EXISTS EstadistikaTotalaBete //
+CREATE TRIGGER EstadistikaTotalaBete
+BEFORE UPDATE ON EstadistikakEgunean
+FOR EACH ROW
+BEGIN
+    UPDATE EstadistikaTotala
+    SET Entzunaldiak =  Entzunaldiak + 1
+    WHERE IdAudio = NEW.IdAudio; 
+END;
+//
