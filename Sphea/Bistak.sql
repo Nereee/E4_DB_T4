71cf2454e639af549bc3d0ce3db5e9d908ac0364
Aitzolgy <157578476+Aitzolgy@users.noreply.github.com>
2024-05-06 11:50:58 +0200

    Update Bistak.sql

--- Sphea/Bistak.sql
+++ Sphea/Bistak.sql
@@ -1,30 +1,16 @@
-
 use Sphea;
 
-drop view if exists EstatistikakAurkestuAbestiak;
-create view EstatistikakAurkestuAbestiak as
-select ar.IzenArtistikoa, au.Izena, er.Egunero, er.Astero, er.Hilero, er.Urtero, er.Beti from Estadistikak er INNER JOIN Audio au on er.IdAudio = au.IdAudio
-INNER JOIN Abestia ab on au.IdAudio = ab.IdAudio
-INNER JOIN Album al ON ab.IdAlbum = al.IdAlbum
-INNER JOIN Artista ar ON al.IdArtista = ar.IdArtista; 
-
-drop view if exists EstatistikakAurkestuPodcast;
-create view EstatistikakAurkestuPodcast as
-select ar.IzenArtistikoa, au.Izena, er.Egunero, er.Astero, er.Hilero, er.Urtero, er.Beti from Estadistikak er INNER JOIN Audio au on er.IdAudio = au.IdAudio
-INNER JOIN Podcast pod on au.IdAudio = pod.IdAudio
-INNER JOIN Podcaster po on pod.IdArtista = po.IdArtista
-INNER JOIN Artista ar on po.IdArtista = ar.IdArtista;
 
 drop view if exists EstatistikakAurkestuMusikariaTotala;
 create view EstatistikakAurkestuMusikariaTotala as
-select ar.IzenArtistikoa AS Izena, sum(er.Beti) as Totala , ar.Irudia as Irudia from Estadistikak er INNER JOIN Audio au on er.IdAudio = au.IdAudio
+select ar.IzenArtistikoa AS Izena, sum(er.Entzunaldiak) as Totala , ar.Irudia as Irudia from EstadistikaTotala er INNER JOIN Audio au on er.IdAudio = au.IdAudio
 INNER JOIN Abestia ab on au.IdAudio = ab.IdAudio
 INNER JOIN Album al ON ab.IdAlbum = al.IdAlbum
 INNER JOIN Artista ar ON al.IdArtista = ar.IdArtista group by ar.IdArtista; 
 
 drop view if exists EstatistikakAurkestuPodcasterraTotala;
 create view EstatistikakAurkestuPodcasterraTotala as
-select ar.IzenArtistikoa as Izena, sum(er.Beti) as Totala, ar.Irudia from Estadistikak er INNER JOIN Audio au on er.IdAudio = au.IdAudio
+select ar.IzenArtistikoa as Izena, sum(er.Entzunaldiak) as Totala, ar.Irudia from  EstadistikaTotala er INNER JOIN Audio au on er.IdAudio = au.IdAudio
 INNER JOIN Podcast pod on au.IdAudio = pod.IdAudio
 INNER JOIN Podcaster po on pod.IdArtista = po.IdArtista
 INNER JOIN Artista ar on po.IdArtista = ar.IdArtista group by ar.IdArtista; 
